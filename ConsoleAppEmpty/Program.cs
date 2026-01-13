f66c5a486faed4ff8c6ab5b0451f0e89e3424f31
Caleb.Perea <cj.perea06@gmail.com>
2026-01-13 15:12:04 -0700

    Everything works

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -1,15 +1,15 @@
-﻿public class Program
+﻿using GameEnums;
+
+
+public class Program
 {
     static void Main(string[] args)
     {
+        Difficulty example = Difficulty.Medium;
+        CharacterClass Character = CharacterClass.Mage;
+        Console.WriteLine($"Selected Difficulty: {example}");
+        Console.WriteLine($"Selected Class: {Character}");
 
-       
     }
 }
-public class GameEnums
-{
 
-}
-{
-    
-}
