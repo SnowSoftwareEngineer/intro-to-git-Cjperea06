73ccad4bf24546ec960e705446be35708b327c0b
Caleb.Perea <cj.perea06@gmail.com>
2026-01-13 14:24:50 -0700

    math

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -8,6 +8,10 @@
         Console.WriteLine("hello world");
         Console.WriteLine("hello world i am megatron");
         Console.WriteLine("hello world i am megamind");
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        Console.WriteLine($"The sum of {x} and {y} is {z}");
 
     }
 }
