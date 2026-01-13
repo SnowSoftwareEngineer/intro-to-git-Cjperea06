48c32bd829dc04674945368ce8e75675c0b6d5cc
caleb.perea <caleb.perea@ENGR3KCP5S3>
2026-01-13 14:51:01 -0700

    enums

---
+++ ConsoleAppEmpty/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEnums
+{
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard,
+    }
+    public enum CharacterClass
+    {
+        Warrior,
+        Mage,
+        Rogue,
+    }
+}
--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -2,20 +2,14 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("hello world");
-        Console.WriteLine("hello world");
-        Console.WriteLine("hello world");
-        Console.WriteLine("hello world");
-        Console.WriteLine("hello world i am megatron");
-        Console.WriteLine("hello world i am megamind");
-        int x = 5;
-        int y = 10;
-        int z = x + y;
-        Console.WriteLine($"The sum of {x} and {y} is {z}");
-        int a = 15;
-        int b = 20;
-        int c = a * b + z;
-        Console.WriteLine($"The result of {a} * {b} + {z} is {c}");
 
+       
     }
+}
+public class GameEnums
+{
+
+}
+{
+    
 }
